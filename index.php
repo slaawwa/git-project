4bd2d76cb8d5061d92763548c048c2551c2fa7bf
slaawwa <slaawwa@mail.ru>
2015-08-17 08:40:24 +0300

    New version

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
 		<title>TEST git</title>
 	</head>
 	<body>
+		<header>
+			<nav>
+				<a href="#home">Home</a> |
+				<a href="#about">About</a>
+			</nav>
+		</header>
 		<h1>HELLO, World!</h1>
 		<p>
 			It`s my first test on the GitHub!
